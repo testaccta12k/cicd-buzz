736b9f2f2d71c86059dbbdbb0bb8b504e7b9ba97
testaccta12k <aaron@a12k.io>
2017-06-23 17:38:55 -0400

    adding words feature

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,6 +1,6 @@
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('does this make sense', 'continuous testing', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
