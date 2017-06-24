e3af3a7729f0bffb855505a8c28fdc05f9baab0b
testaccta12k <aaron@a12k.io>
2017-06-23 20:57:23 -0400

    add test

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -18,3 +18,6 @@ def test_sample_multiple_words():
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
+
+def test_always_pass():
+  assert 1
